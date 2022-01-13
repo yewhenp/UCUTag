d7a78972804c9510bf93461e1f4afe605b670134
Pavlo Hilei <p.hilei@ucu.edu.ua>
2022-01-13 20:09:36 +0200

    Fix clean sript

--- clean.sh
+++ clean.sh
@@ -7,12 +7,12 @@ if [[ -n "${UCUTAG_FILE_DIR}" ]]; then
     echo "Cleaning ${UCUTAG_FILE_DIR}"
     rm -rf ${UCUTAG_FILE_DIR}/*
     echo "Dropping ${UCUTAG_FILE_DIR//\//_}"
-    mongo --eval "db.dropDatabase()" ${UCUTAG_FILE_DIR//\//_}
+    mongo --eval "db.dropDatabase()" "ucutag${UCUTAG_FILE_DIR//\//_}"
 elif [[ -d "/opt/ucutag/files" ]]; then
     echo "Cleaning /opt/ucutag/files"
     rm -rf /opt/ucutag/file/*
-    echo "Dropping _opt_ucutag_files"
-    mongo --eval "db.dropDatabase()" "_opt_ucutag_files"
+    echo "Dropping ucutag_opt_ucutag_files"
+    mongo --eval "db.dropDatabase()" "ucutag_opt_ucutag_files"
 else
     echo "Dont't know where are files to clear"
 fi
