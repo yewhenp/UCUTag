09f7e405107490a0f0824fb0457ffea9b6370066
Pavlo Hilei <p.hilei@ucu.edu.ua>
2022-01-13 22:32:35 +0200

    Revert compile.sh

--- compile.sh
+++ compile.sh
@@ -6,9 +6,6 @@ set -o pipefail
 
 mkdir -p build
 (
-  echo "<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<<"
-  echo ${pkgdir}
-  echo ${pkgdir-"/usr/local/bin"}
   cd build
   cmake -G"Unix Makefiles" -DCMAKE_BUILD_TYPE=${1-Release} -DCMAKE_INSTALL_PREFIX=/usr/local/bin -S ..
   make -j4 X11INC=/usr/include/X11 X11LIB=/usr/lib/X11 VERBOSE=1
