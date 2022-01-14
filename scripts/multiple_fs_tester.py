5137c4f2896c7be5f7bb1fdbeecbbbd9e48fbe4a
Tsapiv Volodymyr <volodja.tsv@gmail.com>
2022-01-15 00:21:16 +0200

    Update scripts

--- scripts/multiple_fs_tester.py
+++ scripts/multiple_fs_tester.py
@@ -7,20 +7,20 @@ def main():
     parser = argparse.ArgumentParser(description="FS benchmark")
     parser.add_argument("--root", dest="dir_root", required=True, type=str)
     parser.add_argument("--tree-dir-width", dest="tree_dir_width", default=3, type=int)
-    parser.add_argument("--fs-type", dest="fs_type", default="ext2", type=str)
+    parser.add_argument("--fs-type", dest="fs_type", default="tag", type=str)
     args = parser.parse_args()
 
     create_dir_time_all, create_files_time_all, access_files_time_all, \
-        search_files_time_all, delete_files_time_all, delete_dirs_time_all = [], [], [], [], [], []
-    tree_heights = [3, 4, 5, 6]
-    tree_file_widths = [5, 6, 7, 8]
+    search_files_time_all, delete_files_time_all, delete_dirs_time_all = [], [], [], [], [], []
+    tree_heights = [2, 3, 4, 5]
+    tree_file_widths = [4, 5, 6, 7, 8]
     for tree_height in tree_heights:
         for tree_file_width in tree_file_widths:
             print(f"Running with tree_height = {tree_height} and tree_file_width = {tree_file_width}")
 
             fs_tester = FSTester(args.dir_root, tree_height, args.tree_dir_width, tree_file_width, args.fs_type)
             create_dir_time, create_files_time, access_files_time, \
-                search_files_time, delete_files_time, delete_dirs_time = fs_tester.count_time()
+            search_files_time, delete_files_time, delete_dirs_time = fs_tester.count_time()
 
             create_dir_time_all.append(create_dir_time)
             create_files_time_all.append(create_files_time)
